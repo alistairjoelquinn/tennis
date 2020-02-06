92b092f94d5f0dfbba0ea4d54a86f48308ec2ef0
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-02-06 13:16:11 +0100

    larger tennis court size and lines drawn

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const canvas = document.getElementById('tennis');
+let userScore = document.getElementById('user');
+let computerScore = document.getElementById('computer');
 const ctx = canvas.getContext('2d');
 
 const user = {
@@ -20,20 +22,44 @@ const computer = {
 }
 
 const net = {
-    x: canvas.width/2 - 1, 
+    x: canvas.width/2 - 2.5, 
+    y: 0,
+    width: 5,
+    height: 15,
+    color: 'darkslategrey',
+}
+
+const line1 = {
+    x: canvas.width/4 - 1, 
     y: 0,
     width: 2,
     height: 15,
-    color: 'dimgray',
+    color: 'white',
+}
+
+const line2 = {
+    x: 3*(canvas.width/4) - 1, 
+    y: 0,
+    width: 2,
+    height: 15,
+    color: 'white',
+}
+
+const horizontal = {
+    x: (canvas.width/4) - 1,
+    y: canvas.height/2, 
+    width: 500,
+    height: 2,
+    color: 'white',
 }
 
 const ball = {
     x: canvas.width/2,
     y: canvas.height/2,
     radius: 10,
-    speed: 5,
-    velocityX: 5,
-    velocityY: 5,
+    speed: 10,
+    velocityX: 10,
+    velocityY: 10,
     color: 'yellow'
 }
 
@@ -63,6 +89,22 @@ const drawNet = () => {
     }
 }
 
+const drawLine1 = () => {
+    for (let i = 0; i <= canvas.height; i+=15) {
+        drawRect(line1.x, line1.y + i, line1.width, line1.height, line1.color);
+    }
+}
+
+const drawLine2 = () => {
+    for (let i = 0; i <= canvas.height; i+=15) {
+        drawRect(line2.x, line2.y + i, line2.width, line2.height, line2.color);
+    }
+}
+
+const drawHorizontal = () => {
+    drawRect(horizontal.x, horizontal.y, horizontal.width, horizontal.height, horizontal.color);
+}
+
 const collision = (b, p) => {
     p.top = p.y;
     p.bottom = p.y + p.height;
@@ -80,18 +122,18 @@ const resetBall = () => {
     ball.velocityY = 0;
     ball.x = canvas.width/2;
     ball.y = canvas.height/2;
-    ball.speed = 5;
+    ball.speed = 10;
     ball.velocityX = -ball.velocityX;
     setTimeout(() => {
-        ball.velocityX = 5;
-        ball.velocityY = 5;
+        ball.velocityX = 10;
+        ball.velocityY = 10;
     }, 1000);
 }
 
 const update = () => {
     ball.x += ball.velocityX;
     ball.y += ball.velocityY;
-    let skill = 0.4;
+    let skill = 0.2;
     computer.y = ball.y - ((computer.y + computer.height/2) * skill);
     if(ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0) {
         ball.velocityY = -ball.velocityY;
@@ -104,20 +146,21 @@ const update = () => {
         let ballDir = (ball.x < canvas.width/2) ? 1 : -1;
         ball.velocityX = ball.speed * Math.cos(angle) * ballDir;
         ball.velocityY = ball.speed * Math.sin(angle) * ballDir;
-        if(Math.floor(Math.random()) > 0.8) {
+        if(Math.random() > 0.8) {
             ball.speed += 0.1;
         }
-        if(Math.floor(Math.random()) < 0.05) {
-            ball.speed += 0.5;
+        if(Math.random() < 0.05) {
+            ball.speed -= Math.random();
         }
-        ball.speed += Math.floor(Math.random());
-        ball.speed -= Math.floor(Math.random());
+        ball.speed += Math.random();
     }
     if(ball.x - ball.radius < 0) {
         computer.score++;
+        computerScore.innerText = computer.score;
         resetBall();
     } else if(ball.x + ball.radius > canvas.width) {
         user.score++;
+        userScore.innerText = user.score;
         resetBall();
     }
 }
@@ -131,8 +174,9 @@ canvas.addEventListener('mousemove', moveBat);
 
 const render = () => {
     drawRect(0, 0, canvas.width, canvas.height, "#7cfc00");
-    drawText(user.score, canvas.width/4, canvas.width/5, "white");
-    drawText(computer.score, 3*canvas.width/4, canvas.width/5, "white");
+    drawLine1();
+    drawLine2();
+    drawHorizontal();
     drawNet();
     drawRect(user.x, user.y, user.width, user.height, user.color);
     drawRect(computer.x, computer.y, computer.width, computer.height, computer.color);
